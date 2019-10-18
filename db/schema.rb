6e2a814c399dfec8509031d1beef741236bca24f
NakatsuboYusuke <>
2019-10-18 18:42:49 +0900

    Revert "Revert "Revert "ステップ8(E2Eテストを書こう)の内容に併せて、テストファイルを実装しました"""
    This reverts commit d193bc17e18cbef04898276ebaf3168752f2354e.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_012049) do
+ActiveRecord::Schema.define(version: 2019_10_16_060541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title", limit: 30, null: false
-    t.text "description", null: false
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
