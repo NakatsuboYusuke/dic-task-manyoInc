938646271c8de222efbe6d37c669ed7b531a2ec3
NakatsuboYusuke <>
2019-10-21 11:21:46 +0900

    ステップ8までの作業を整理しました

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,38 +1,29 @@
 require 'rails_helper'
 
 RSpec.feature 'タスク管理機能', type: :feature do
-  background do
-    @task = FactoryBot.create(:task, created_at: Time.now + 2.days)
-    @second_task = FactoryBot.create(:second_task, created_at: Time.now + 1.days)
-    @third_task = Task.create(title: 'man', description: 'manmanmanman', created_at: Time.now)
-  end
-
+  
   scenario 'タスク一覧のテスト' do
+    Task.create!(title: 'test_task_01', description: 'testtesttest')
+    Task.create!(title: 'test_task_02', description: 'samplesample')
     visit tasks_path
     expect(page).to have_content 'testtesttest'
-    expect(page).to have_content 'mofmofmofmof'
+    expect(page).to have_content 'samplesample'
   end
 
-  scenario 'タスク作成のテスト', skip: true do
+  scenario 'タスク作成のテスト' do
     visit new_task_path
-    fill_in  '名称', with: 'test_task_01'
-    fill_in  '詳しい説明', with: 'testtesttest'
+    fill_in  'Title', with: 'test_task_01'
+    fill_in  'Description', with: 'testtesttest'
     click_button 'Create Task'
     expect(page).to have_content 'test_task_01'
     expect(page).to have_content 'testtesttest'
   end
 
-  scenario 'タスク詳細のテスト', skip: true do
+  scenario 'タスク詳細のテスト' do
+    Task.create!(title: 'test_task_01', description: 'testtesttest')
     visit tasks_path
     click_link '詳細'
     expect(page).to have_content 'test_task_01'
   end
 
-  scenario 'タスクが作成日時の降順に並んでいるかのテスト' do
-    visit tasks_path
-    @tasks = all('tbody tr')
-    expect(@tasks[0]).to have_content 'test'
-    expect(@tasks[2]).to have_content 'man'
-    save_and_open_page
-  end
 end
