27ab26b2009e86ef61d46dc28e27b8c3c07b892c
NakatsuboYusuke <>
2019-10-21 20:11:29 +0900

    ベーシック認証を実装しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+
+  before_action :basic
+
+  private
+
+  def basic
+    name = 'hoge'
+    passwd = 'e86797b125848e625d70987c20e4127bbb3db51a'
+    authenticate_or_request_with_http_basic('BA') do |n, p|
+      n == name && Digest::SHA1.hexdigest(p) == passwd
+    end
+  end
+
 end
