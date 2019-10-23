3a02f3bd07d23541d6b61d1cc5e5ea0b2436c98f
NakatsuboYusuke <>
2019-10-23 17:40:47 +0900

    ベーシック認証の設定を変更しました

--- spec/features/task_spec.rb
+++ spec/features/task_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.feature 'タスク管理機能', type: :feature do
-  background do
-    FactoryBot.create(:task, created_at: Time.now + 2.days)
-    FactoryBot.create(:second_task, created_at: Time.now + 1.days)
-    FactoryBot.create(:third_task, created_at: Time.now)
-  end
+  #background do
+    #FactoryBot.create(:task, created_at: Time.now + 2.days)
+    #FactoryBot.create(:second_task, created_at: Time.now + 1.days)
+    #FactoryBot.create(:third_task, created_at: Time.now)
+  #end
 
   scenario 'タスク一覧のテスト', skip: true do
     visit tasks_path
@@ -13,10 +13,10 @@ RSpec.feature 'タスク管理機能', type: :feature do
     expect(page).to have_content 'mofmofmofmof'
   end
 
-  scenario 'タスク作成のテスト', skip: true do
+  scenario 'タスク作成のテスト' do
     visit new_task_path
-    fill_in  '名称', with: 'test_task_01'
-    fill_in  '詳しい説明', with: 'testtesttest'
+    fill_in '名称', with: 'test_task_01'
+    fill_in '詳しい説明', with: 'testtesttest'
     click_button 'Create Task'
     expect(page).to have_content 'test_task_01'
     expect(page).to have_content 'testtesttest'
@@ -28,11 +28,20 @@ RSpec.feature 'タスク管理機能', type: :feature do
     expect(page).to have_content 'test_task_01'
   end
 
-  scenario 'タスクが作成日時の降順に並んでいるかのテスト' do
+  scenario 'タスクが作成日時の降順に並んでいるかのテスト', skip: true do
     visit tasks_path
     @tasks = all('tbody tr')
     expect(@tasks[0]).to have_content 'test'
     expect(@tasks[2]).to have_content 'man'
     save_and_open_page
   end
+
+  #scenario '日時の作成のテスト' do
+    #visit new_task_path
+    #fill_in  '名称', with: 'test_task_01'
+    #fill_in  '詳しい説明', with: 'testtesttest'
+    #fill_in datetimeinput, with: '1990-12-31T23:59Z'
+    #click_button 'Create Task'
+    #expect(page).to have_content '1990-12-31T23:59Z'
+  #end
 end
